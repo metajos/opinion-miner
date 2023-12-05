af0b34e11eb0a175be915079d1eb36ed7fb28f0a
Jos Singer <js3866@bath.ac.uk>
2023-12-05 08:16:20 +0000

    finished tuples editing

---
+++ opinionmining.py
@@ -0,0 +1,19 @@
+import sklearn 
+import numpy as np
+import os
+from icecream import ic
+
+
+basepath = os.getcwd()
+
+
+def read_data(file):
+    with open()
+
+def pos_tagger():
+    pass
+    
+
+
+
+
---
+++ testing/pos_test.py
@@ -0,0 +1,12 @@
+import unittest
+from unittest import TestCase
+
+
+class POSTaggerTestCase(TestCase):
+    def test_pos(self):
+        self.assertEqual("A", "A")
+        print("Hello Tests")
+    
+if __name__ == "__main__":
+    unittest.main()
+
